3a3effbc5b9c7453891c8dd8a58c221e20c5aa2a
katia <katiasilvarodrigues960@gmail.com>
2025-01-07 09:47:04 -0300

    Primeiro Commit - projeto aplee

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function trocacor(cor){
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+function trocarimg(img){
+    let imgIphone = document.querySelector(".img-celular")
+    imgIphone.src = img
+}
